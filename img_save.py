960a6e5d2894ec8de6534e841d450bb16490c3dc
bpmagallon <bpmagallon@gmail.com>
2016-03-17 15:40:28 +0900

    Update img_save.py

--- img_save.py
+++ img_save.py
@@ -4,9 +4,9 @@ import matplotlib.image
 def img(array, add):
     img = array[0]
     mfc = array[1]
-    add = add.split(".")
     if array[1]!="MFC":
-        matplotlib.image.imsave(add[0], image_dn, cmap="gray", format=add[1])
+
+        matplotlib.image.imsave(add, img, cmap="gray", format="png")
     else:
-        img = Image.fromarray(rgbArray)
-        img.save(add[0]+"."+add[1])
+        img_mfc = Image.fromarray(img.astype('uint8'))
+        img_mfc.save(add)
