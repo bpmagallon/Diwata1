92623a12bc4b8ef73409827ebfd53d8bedecb1e6
bpmagallon <bpmagallon@gmail.com>
2016-05-02 02:50:46 +0900

    Update imgview.py

--- imgview.py
+++ imgview.py
@@ -2,10 +2,11 @@ import commands
 
 print "Commands:\n"
 print "-bin (bin file address) reads bin file"
-print "-toa (gain) (exp time(s))convert DN to TOA Radiance"
+print "-toa convert DN to TOA Radiance"
 print "-img (image output filename+img format) converts bin to image file"
 print "-att (attitude data address) gives location to image"
 print "-footprint (address) creates footprint of image file"
+print "-status get status of the system"
 print "\nNote: No space on file address"
 
 print "Enter command: "
